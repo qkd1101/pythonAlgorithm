492d61bda58139f20e245329e86a3c7ac9946b14
ju young jeong <39512149+qkd1101@users.noreply.github.com>
2021-10-20 17:45:22 +0900

    commit

--- test.py
+++ test.py
@@ -1,16 +1,9 @@
-# a = [(2,4),(2,3),(4,8),(5,5),(5,7),(6,7)]
-
-# a = sorted(a, key = lambda x: x[0])
-# a = sorted(a, key = lambda x: x[1])
-
-# print(a)
 import sys
 
-n = input().split()
-a = list(map(int,input().split()))
-cnt = 0
+N = int(sys.stdin.readline())
 
-for k in range(n):
-    if a[k] = a[k-1]+1:
-        cnt += 1
+K = list(map(int, input().split()))
+P = list(map(int, input().split()))
 
+for k in range(N-1,0,-1):
+    
