6183d3a7c9e679918ccdcc2e38307531d18f2b1b
ju young jeong <39512149+qkd1101@users.noreply.github.com>
2021-10-24 14:53:17 +0900

    commit
    comiit

--- test.py
+++ test.py
@@ -1,9 +1,29 @@
 import sys
 
-N = int(sys.stdin.readline())
+n, m = int(input().split())
 
-K = list(map(int, input().split()))
-P = list(map(int, input().split()))
+start_point_x = n
+start_point_y = 0
 
-for k in range(N-1,0,-1):
-    
+cnt = 1
+
+if n >= m:
+    while(True):
+        if start_point_x - 2 => 0 and start_point_y + 1 <= m:
+            start_point_x -= 2
+            start_point_y += 1
+            cnt += 1
+        elif start_point_x + 2 =< n and start_point_y + 1 <= m:
+            start_point_x += 2
+            start_point_y += 1
+            cnt += 1
+        elif start_point_x - 1 => 0 and start_point_y + 2 <= m:
+            start_point_x -= 1
+            start_point_y += 2
+            cnt += 1
+        elif start_point_x  + 1 =< n  and start_point_y <= m - 2:
+            start_point_x -= 1
+            start_point_y += 2
+            cnt += 1
+
+else :
